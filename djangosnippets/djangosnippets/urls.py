1ad2e4f81852829f45da62efd8162ad4feebfa44
YS office LLC <yusuke.sato@ys-office.me>
2021-07-22 20:10:57 +0900

    ルーティング設定後にテスト通過

--- djangosnippets/djangosnippets/urls.py
+++ djangosnippets/djangosnippets/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from snippets.views import top
 
 urlpatterns = [
+    path('', top, name='top'),
     path('admin/', admin.site.urls),
 ]
