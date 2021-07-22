bc2ebf612f03098ea67bde5ab49de467e6768cd0
YS office LLC <yusuke.sato@ys-office.me>
2021-07-22 20:33:01 +0900

    スニペット登録・詳細・編集用 View 登録、テスト実装

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,4 +1,25 @@
 from django.test import TestCase
+from django.urls import resolve
+
+
+from snippets.views import top, snippet_new, snippet_edit, snippet_detail
+
+
+class CreateSnippetTest(TestCase):
+    def test_should_resolve_snippet_new(self):
+        found = resolve("/snippets/new/")
+        self.assertEqual(snippet_new, found.func)
+
+
+class SnippetDetailTest(TestCase):
+    def test_should_resolve_snippet_detail(self):
+        found = resolve("/snippets/1")
+        self.assertEqual(snippet_detail, found.func)
+
+class EditSnippetTest(TestCase):
+    def test_should_resolve_snippet_edit(self):
+        found = resolve("/snippets/1/edit/")
+        self.assertEqual(snippet_edit, found.func)
 
 
 class TopPageTest(TestCase):
