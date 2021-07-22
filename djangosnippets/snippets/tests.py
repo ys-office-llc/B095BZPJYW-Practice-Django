9676c855cbf628167c1d4851d2471f28181bc0d4
YS office LLC <yusuke.sato@ys-office.me>
2021-07-22 20:08:12 +0900

    ルーティング設定前に Client クラスの HTTP リクエストで 404 エラーを出したテスト

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,16 +1,11 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from snippets.views import top
 
 
-class TopPageViewTest(TestCase):
+class TopPageTest(TestCase):
     def test_top_returns_200(self):
-        request = HttpRequest()
-        response = top(request)
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-
     def test_top_returns_expected_content(self):
-        request = HttpRequest()
-        response = top(request)
+        response = self.client.get("/")
         self.assertEqual(response.content, b"Hello World")
