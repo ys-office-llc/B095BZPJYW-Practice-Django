079d0274cbbd2f8e50a9ab6351f92eed3186f179
YS office LLC <yusuke.sato@ys-office.me>
2021-07-24 18:31:55 +0900

    1.4.4 - スニペットの登録・編集ページ - HTTP メソッドを正しくハンドリングする

--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseForbidden
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_safe, require_http_methods
 
 
 from snippets.models import Snippet
 from snippets.forms import SnippetForm
 
 
+@require_safe ### GET / HEAD メソッドのみ受け付ける
 def top(request):
     snippets = Snippet.objects.all()
     context = {"snippets": snippets}
@@ -15,6 +17,7 @@ def top(request):
 
 
 @login_required
+@require_http_methods(["GET", "POST", "HEAD"])
 def snippet_new(request):
     if request.method == 'POST':
         form = SnippetForm(request.POST)
@@ -29,6 +32,7 @@ def snippet_new(request):
 
 
 @login_required
+@require_http_methods(["GET", "POST", "HEAD"])
 def snippet_edit(request, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
     if snippet.created_by_id != request.user.id:
@@ -43,6 +47,7 @@ def snippet_edit(request, snippet_id):
         return render(request, 'snippets/snippet_edit.html', {'form': form})
 
 
+@require_safe
 def snippet_detail(request, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
     return render(request, 'snippets/snippet_detail.html', {'snippet': snippet})
