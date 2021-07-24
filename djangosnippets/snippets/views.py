98bfef32012cd523f59567f8cf9909d074fb7737
YS office LLC <yusuke.sato@ys-office.me>
2021-07-24 18:25:41 +0900

    1.4.4 - スニペットの登録・編集ページ - リスト 1.27, 28, 29, 30, 31 実装後にテスト成功

--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponseForbidden
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 from snippets.models import Snippet
+from snippets.forms import SnippetForm
 
 
 def top(request):
@@ -11,12 +14,33 @@ def top(request):
     return render(request, "snippets/top.html", context)
 
 
+@login_required
 def snippet_new(request):
-    return HttpResponse('スニペットの登録')
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            snippet = form.save(commit=False)
+            snippet.created_by = request.user
+            snippet.save()
+            return redirect(snippet_detail, snippet_id=snippet.pk)
+    else:
+        form = SnippetForm()
+        return render(request, "snippets/snippet_new.html", {'form': form})
 
 
+@login_required
 def snippet_edit(request, snippet_id):
-    return HttpResponse('スニペットの編集')
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    if snippet.created_by_id != request.user.id:
+        return HttpResponseForbidden("このスニペットの編集は許可されていません。")
+    if request.method == 'POST':
+        form = SnippetForm(request.POST, instance=snippet)
+        if form.is_valid():
+            form.save()
+            return redirect('snippet_detail', snippet_id=snippet_id)
+    else:
+        form = SnippetForm(instance=snippet)
+        return render(request, 'snippets/snippet_edit.html', {'form': form})
 
 
 def snippet_detail(request, snippet_id):
