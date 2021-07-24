de419cff0dcfb570f5c2b5a000cf42e23f09d98d
YS office LLC <yusuke.sato@ys-office.me>
2021-07-24 10:53:27 +0900

    1.4.2 - トップページにスニペットの一覧を表示 - リスト 1.18 snippets/tests.py - テスト追加、失敗させる

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,8 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
 from django.urls import resolve
+from django.contrib.auth import get_user_model
 
 
+from snippets.models import Snippet
 from snippets.views import top, snippet_new, snippet_edit, snippet_detail
+
+
+UserModel = get_user_model()
+
+class TopPageRenderSnippetsTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="top_secret_pass0001",
+        )
+        self.snippet = Snippet.objects.create(
+            title="title1",
+            code="print('hello')",
+            description="description1",
+            created_by=self.user,
+        )
+
+    def test_should_return_snippet_title(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.asserContains(response, self.snippet.title)
+
+    def test_should_return_snippet_username(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.asserContains(response, self.user.username)
 
 
 class CreateSnippetTest(TestCase):
