aa2b7a598ff63ba2453e10cd99d372d048b45d54
YS office LLC <yusuke.sato@ys-office.me>
2021-07-24 17:33:30 +0900

    1.4.4 - スニペットの登録・編集ページ - リスト 1.26 snippets/tests.py 編集後にテスト失敗

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -37,9 +37,25 @@ class TopPageRenderSnippetsTest(TestCase):
 
 
 class CreateSnippetTest(TestCase):
-    def test_should_resolve_snippet_new(self):
-        found = resolve("/snippets/new/")
-        self.assertEqual(snippet_new, found.func)
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="secret",
+        )
+        self.client.force_login(self.user)
+
+    def test_render_creation_form(self):
+        response = self.client.get("/snippets/new/")
+        self.assertContains(response, "スニペットの登録", status_code=200)
+
+    def test_create_snippet(self):
+        data = {'title': 'タイトル', 'code': 'コード', 'description': '解説'}
+
+        self.client.post("/snippets/new/", data)
+        snippet = Snippet.objects.get(title='タイトル')
+        self.assertEqual('コード', snippet.code)
+        self.assertEqual('解説', snippet.description)
 
 
 class SnippetDetailTest(TestCase):
