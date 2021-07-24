58c8e31583269597e28e80e05db82cd2f0d8d57d
YS office LLC <yusuke.sato@ys-office.me>
2021-07-24 13:04:58 +0900

    1.4.3 - スニペットの詳細ページ - リスト 1.24 修正 1.25 追加後テスト成功

--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 from snippets.models import Snippet
@@ -20,4 +20,5 @@ def snippet_edit(request, snippet_id):
 
 
 def snippet_detail(request, snippet_id):
-    return HttpResponse('スニペットの詳細閲覧')
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    return render(request, 'snippets/snippet_detail.html', {'snippet': snippet})
